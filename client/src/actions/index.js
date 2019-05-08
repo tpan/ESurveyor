be4cbd5ef1da379e7d344fb984c3e95150b7c05f
Thomas Pan <thomastpan@gmail.com>
2019-05-08 14:08:38 -0700

    reducer functional

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -4,5 +4,5 @@ import { FETCH_USER } from './types';
 // fetch user model for current logged in user
 export const fetchUser = () => async dispatch => {
 	const res = await axios.get('/api/current_user');
-	dispatch({ type: FETCH_USER, payload: res });
+	dispatch({ type: FETCH_USER, payload: res.data });
 };
--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -20,27 +20,33 @@ class Header extends Component {
 	// 			);
 	// 	}
 	// }
-
+	renderContent() {
+		switch (this.props.auth) {
+			case null:
+				return 'stilldeciding';
+			case false:
+				return 'im logged out';
+			default:
+				return 'im logged in';
+		}
+	}
 	render() {
+		console.log(this.props);
 		return (
 			<nav>
 				<div className='nav-wrapper'>
 					<a href='/' className='left brand-logo'>
 						Emaily
 					</a>
-					<ul className='right'>
-						<li>
-							<a>Login with google</a>
-						</li>
-					</ul>
+					<ul className='right'>{this.renderContent()}</ul>
 				</div>
 			</nav>
 		);
 	}
 }
-//Accessing redux store in Header component
-// function mapStateToProps({ auth }) {
-// 	return { auth };
-// }
+// Accessing redux store in Header component
+function mapStateToProps({ auth }) {
+	return { auth };
+}
 
-export default Header;
+export default connect(mapStateToProps)(Header);

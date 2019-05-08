4c005209abf0b143700005d0334177ff48492bbc
Thomas Pan <thomastpan@gmail.com>
2019-05-08 12:35:14 -0700

    header, react router done

---
+++ client/src/components/Header.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+class Header extends Component {
+	renderContent() {
+		switch (this.props.auth) {
+			case null:
+				return;
+			case false:
+				return (
+					<li>
+						<a href='/auth/google'>Login With Google</a>
+					</li>
+				);
+			default:
+				return (
+					<li>
+						<a href='/api/logout'>Logout</a>
+					</li>
+				);
+		}
+	}
+
+	render() {
+		return (
+			<nav>
+				<div className='nav-wrapper'>
+					<a href='/' className='left brand-logo'>
+						Emaily
+					</a>
+					<ul className='right'>{this.renderContent()}</ul>
+				</div>
+			</nav>
+		);
+	}
+}
+//Accessing redux store in Header component
+function mapStateToProps({ auth }) {
+	return { auth };
+}
+
+export default connect(mapStateToProps)(Header);

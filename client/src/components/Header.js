394ef7ff169dd751eb2bb72c047b0eff08421228
Thomas Pan <thomastpan@gmail.com>
2019-05-08 12:56:22 -0700

    added materialize css

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -2,24 +2,24 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 class Header extends Component {
-	renderContent() {
-		switch (this.props.auth) {
-			case null:
-				return;
-			case false:
-				return (
-					<li>
-						<a href='/auth/google'>Login With Google</a>
-					</li>
-				);
-			default:
-				return (
-					<li>
-						<a href='/api/logout'>Logout</a>
-					</li>
-				);
-		}
-	}
+	// renderContent() {
+	// 	switch (this.props.auth) {
+	// 		case null:
+	// 			return;
+	// 		case false:
+	// 			return (
+	// 				<li>
+	// 					<a href='/auth/google'>Login With Google</a>
+	// 				</li>
+	// 			);
+	// 		default:
+	// 			return (
+	// 				<li>
+	// 					<a href='/api/logout'>Logout</a>
+	// 				</li>
+	// 			);
+	// 	}
+	// }
 
 	render() {
 		return (
@@ -28,15 +28,19 @@ class Header extends Component {
 					<a href='/' className='left brand-logo'>
 						Emaily
 					</a>
-					<ul className='right'>{this.renderContent()}</ul>
+					<ul className='right'>
+						<li>
+							<a>Login with google</a>
+						</li>
+					</ul>
 				</div>
 			</nav>
 		);
 	}
 }
 //Accessing redux store in Header component
-function mapStateToProps({ auth }) {
-	return { auth };
-}
+// function mapStateToProps({ auth }) {
+// 	return { auth };
+// }
 
-export default connect(mapStateToProps)(Header);
+export default Header;

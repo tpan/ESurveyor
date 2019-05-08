ec3b3376a1d2363a4ed271a8cf84e4553fe457c1
Thomas Pan <thomastpan@gmail.com>
2019-05-08 13:07:08 -0700

    adding fetchuser

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -4,6 +4,5 @@ import { FETCH_USER } from './types';
 // fetch user model for current logged in user
 export const fetchUser = () => async dispatch => {
 	const res = await axios.get('/api/current_user');
-
 	dispatch({ type: FETCH_USER, payload: res });
 };

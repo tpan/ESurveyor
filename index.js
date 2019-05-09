dd9f177b75edb13de19a19cedf88e602b8ec2627
Thomas Pan <thomastpan@gmail.com>
2019-05-09 00:17:04 -0700

    added credits\/finished stripe integration

--- index.js
+++ index.js
@@ -3,12 +3,14 @@ const mongoose = require('mongoose');
 const passport = require('passport');
 const cookieSession = require('cookie-session');
 const keys = require('./config/keys');
+const bodyParser = require('body-parser');
 require('./models/User');
 require('./services/passport');
 
 mongoose.connect(keys.mongoURI);
 const app = express();
 
+app.use(bodyParser.json());
 app.use(
 	cookieSession({
 		maxAge: 30 * 24 * 60 * 60 * 1000,

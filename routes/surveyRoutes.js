8d5f2c63a9d73f998f8cf1573d622c4522a09d8e
Thomas Pan <thomastpan@gmail.com>
2019-05-13 00:38:24 -0700

    redirect url

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -5,8 +5,11 @@ const Mailer = require('../services/mailer')
 const surveyTemplate = require('../services/emailTemplates/surveyTemplate')
 
 const Survey = mongoose.model('surveys')
-
+//TODO add custom redirect url/thanks page depending on given url; Depending on feedback?
 module.exports = app => {
+	app.get('/api/surveys/thanks', (req, res) => {
+		res.send('Thanks for your feedback!')
+	})
 	app.post('/api/surveys', requireLogin, minCredits, async (req, res) => {
 		const { title, subject, body, recipients } = req.body
 		const survey = new Survey({
